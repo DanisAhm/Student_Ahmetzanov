812cc15962a18dc84500d8f13e97d0e66a0e7144
Данис <ahmetzanovdanis256@gmail.com>
2025-01-20 20:35:23 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const MyApp = () => {
+  return (
+    <View style = {{ flex: 1, justifyContent: "center", allignItems: "center" }}>
+      <Text>
+        Hello World
+      </Text>
+    </View>
+  )
+}
+
+export default MyApp;
